bcd6e4a5b45ce535c8269caa48e6af3242078d53
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-05 19:09:34 -0300

    feat: componentize app on a new file

--- server.ts
+++ server.ts
@@ -1,13 +1,9 @@
 import './src/config/setup';
 import './src/config/database';
-import express, { json } from 'express';
-import cors from 'cors';
 import chalk from 'chalk';
+import app from './src/app';
 
-const app = express();
-app.use(json());
-app.use(cors());
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || '5000';
 
 app.listen(port, () => {
   console.log(chalk.bold.green(`Server running on http://localhost:${port}`));
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+// import router from './routes/index.js';
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use(router);
+app.use(errorHandlerMiddleware);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to the TRACTIAN API');
+});
+
+export default app;
