d41446cd05e02040b72b7c1ae81b722dc2f0b9b8
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-05 16:57:49 -0300

    feat: create user service

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -5,24 +5,24 @@ import { ObjectId } from 'mongodb';
 export type CreateUserData = Omit<User, '_id' | 'createdAt' | 'updatedAt'>;
 
 export default class UserRepository {
-  static async create(createUserData: CreateUserData) {
+  public async create(createUserData: CreateUserData) {
     const user = await db.collection('users').insertOne(createUserData);
     return user.insertedId;
   }
 
-  static async findByEmail(email: string) {
+  public async findByEmail(email: string) {
     const user = await db.collection('users').findOne({ email });
     return user;
   }
 
-  static async findById(id: string) {
+  public async findById(id: string) {
     const user = await db
       .collection('users')
       .findOne({ _id: new ObjectId(id) });
     return user;
   }
 
-  static async update(id: string, updateUserData: CreateUserData) {
+  public async update(id: string, updateUserData: CreateUserData) {
     const user = await db
       .collection('users')
       .findOneAndUpdate({ _id: new ObjectId(id) }, { $set: updateUserData });
---
+++ src/services/user.service.ts
@@ -0,0 +1,58 @@
+import UserRepository, {
+  CreateUserData,
+} from '../repositories/user.repository';
+import AppError from '../utils/AppError';
+import * as AuthUtils from '../utils/auth';
+
+export default class UserService {
+  userRepository: UserRepository;
+
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  public async create(createUserData: CreateUserData) {
+    await this.verifyIfUserExists(createUserData.email);
+    const hashedPassword = AuthUtils.encryptPassword(createUserData.password);
+    const userId = await this.userRepository.create({
+      ...createUserData,
+      password: hashedPassword,
+    });
+    return userId;
+  }
+
+  async findByEmail(email: string) {
+    return this.userRepository.findByEmail(email);
+  }
+
+  async findById(id: string) {
+    return this.userRepository.findById(id);
+  }
+
+  async update(id: string, updateUserData: CreateUserData) {
+    return this.userRepository.update(id, updateUserData);
+  }
+
+  async verifyPassword(password: string, hashedPassword: string) {
+    return AuthUtils.verifyPassword(password, hashedPassword);
+  }
+
+  async verifyIfUserExists(email: string) {
+    const user = await this.userRepository.findByEmail(email);
+    if (user) {
+      throw new AppError('User already exists', 400);
+    }
+  }
+
+  async verifyCredentials(email: string, password: string) {
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('Invalid credentials', 401);
+    }
+    const isPasswordValid = await this.verifyPassword(password, user.password);
+    if (!isPasswordValid) {
+      throw new AppError('Invalid credentials', 401);
+    }
+    return user;
+  }
+}
