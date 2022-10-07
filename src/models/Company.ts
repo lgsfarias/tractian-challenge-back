c7ef156ba5846e5f9341fa7a5ace7d7e5f40e80a
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-07 12:38:15 -0300

    feat: create company repository

---
+++ src/models/Company.ts
@@ -0,0 +1,11 @@
+import { Schema, model } from 'mongoose';
+import { Company } from '../interfaces';
+
+const companySchema = new Schema<Company>({
+  name: { type: String, required: true },
+  units: [{ type: Schema.Types.ObjectId, ref: 'units', default: [] }],
+});
+
+const companies = model<Company>('companies', companySchema);
+
+export default companies;
---
+++ src/repositories/company.repository.ts
@@ -0,0 +1,30 @@
+import { Company } from '../interfaces';
+import companies from '../models/Company';
+
+export type CreateCompanyData = Omit<Company, 'createdAt'>;
+
+export default class CompanyRepository {
+  public async create(createCompanyData: CreateCompanyData) {
+    const company = await companies.create(createCompanyData);
+    return company._id;
+  }
+
+  public async findById(id: string) {
+    const company = await companies.findById(id);
+    return company;
+  }
+
+  public async findAll() {
+    const companiesList = await companies.find();
+    return companiesList;
+  }
+
+  public async update(id: string, updateCompanyData: CreateCompanyData) {
+    const company = await companies.findByIdAndUpdate(id, updateCompanyData);
+    return company;
+  }
+
+  public async delete(id: string) {
+    await companies.findByIdAndDelete(id);
+  }
+}
