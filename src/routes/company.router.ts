c6880dfa68f8917c0688374c1374d588bf9321fc
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-10-07 13:07:36 -0300

    feat: create company routes

---
+++ src/routes/company.router.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import CompanyController from '../controllers/company.controller';
+import validateSchemaMiddleware from '../middlewares/validadeSchemaMiddleware';
+import verifyTokenMiddleware from '../middlewares/verifyTokenMiddleware';
+import * as schemas from '../schemas';
+
+const companyController = new CompanyController();
+const companyRouter = Router();
+
+companyRouter.post(
+  '/',
+  verifyTokenMiddleware,
+  validateSchemaMiddleware(schemas.addCompany),
+  (req, res) => companyController.create(req, res),
+);
+
+companyRouter.get('/', (req, res) => companyController.list(req, res));
+
+companyRouter.get('/:id', (req, res) => companyController.findById(req, res));
+
+companyRouter.put('/:id', verifyTokenMiddleware, (req, res) =>
+  companyController.update(req, res),
+);
+
+companyRouter.delete('/:id', verifyTokenMiddleware, (req, res) =>
+  companyController.delete(req, res),
+);
+
+export default companyRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import auth from './auth.router';
+import company from './company.router';
 
 const router = Router();
 
 router.use('/', auth);
+router.use('/companies', company);
 
 export default router;
